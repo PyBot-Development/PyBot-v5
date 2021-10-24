f3bab95af6199a7bd714db7a4857570e04c8fcaa
mariyt10 <mariuszbubak10@gmail.com>
2021-10-24 15:23:58 +0200

    Sussy

--- src/cogs/commands/help.py
+++ src/cogs/commands/help.py
@@ -17,13 +17,22 @@ from discord.ext.commands import cooldown, BucketType, CommandNotFound
 
 
 class HelpButtons(discord.ui.View):
-    def __init__(self, maxPages, commands, author):
+    def __init__(self, client, author):
         super().__init__(timeout=20)
+        self.client = client
         self.page = 0
-        self.maxPages = maxPages
-        self.commands = commands
         self.author = author
         self.message = None
+
+        commands = ''.join(
+            f"{support.prefix}{command}\n​   `{command.description}`\n" for command in client.commands).splitlines()
+        self.commands = []
+        n = 10
+        for index in range(0, len(commands), n):
+            page = ''.join(f"{item}\n" for item in commands[index: index + n])
+            self.commands.append(page)
+
+        self.maxPages = int(len(self.commands)) - 1
 
     async def on_timeout(self) -> None:
         self.back.disabled = True
@@ -123,28 +132,19 @@ class HelpButtons(discord.ui.View):
 class help(commands.Cog):
     def __init__(self, client):
         self.client = client
-        commands = ''.join(
-            f"{support.prefix}{command}\n​   `{command.description}`\n" for command in self.client.commands).splitlines()
-        self.commands = []
-        n = 10
-        for index in range(0, len(commands), n):
-            page = ''.join(f"{item}\n" for item in commands[index: index + n])
-            self.commands.append(page)
-
-        self.maxPages = int(len(self.commands)) - 1
 
     @cooldown(1, support.cooldown, BucketType.user)
     @commands.command(aliases=["?"], description="Help Command")
     async def help(self, ctx, *, command=None):
-        view = HelpButtons(self.maxPages, self.commands, ctx.message.author)
+        view = HelpButtons(self.client, ctx.message.author)
         message = await ctx.send(embed=discord.Embed(
             title="Help",
             description=f"""
 [Website](https://py-bot.cf/) | [Commands](https://py-bot.cf/commands) | [PyBot's Discord Server](https://discord.gg/dfKMTx9Eea)
 
-{self.commands[0]}""",
+{view.commands[0]}""",
             color=support.colours.default
-        ).set_footer(text=f"Page: 1/{self.maxPages+1}"), view=view)
+        ).set_footer(text=f"Page: 1/{view.maxPages+1}"), view=view)
         view.message = message
 
 def setup(client):
