e63498456e91ee923ce1b21bdb764d13079616df
mariyt10 <mariuszbubak10@gmail.com>
2021-11-29 17:55:54 +0100

    Troll

--- src/cogs/commands/pay.py
+++ src/cogs/commands/pay.py
@@ -18,6 +18,7 @@ from cogs import checks
 class pay(commands.Cog):
     def __init__(self, client):
         self.client = client
+        
     @checks.default()
     @cooldown(1, support.cooldown, BucketType.user)
     @commands.command(description="commands.pay.description")
