749294261256e142a2d29554cc69975e4e22c576
mariyt10 <69349269+M2rsho@users.noreply.github.com>
2022-01-01 22:48:47 +0100

    Version

--- src/run.py
+++ src/run.py
@@ -14,7 +14,7 @@ __title__ = 'Pybot V5'
 __author__ = 'M2rsho'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-2022 M2rsho'
-__version__ = '1.7.2'
+__version__ = '1.7.3'
 
 from discord.ext import commands
 import discord
