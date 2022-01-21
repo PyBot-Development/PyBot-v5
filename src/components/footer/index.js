c3ea455c4d9674f07d1038141df26ae22d68f206
mariyt10 <69349269+M2rsho@users.noreply.github.com>
2022-01-21 22:08:46 +0100

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,7 @@ class Footer extends react.Component {
       <div className="bottom">
           <div className="footer">
             <a href="https://discord.gg/v3ATrV72B6" target="_blank" rel="noreferrer"> <SiDiscord></SiDiscord> </a>
-            <a href="https://github.com/m2rsho" target="_blank" rel="noreferrer"> <SiGithub></SiGithub> </a>
+            <a href="https://github.com/PyBot-Development" target="_blank" rel="noreferrer"> <SiGithub></SiGithub> </a>
             <a href="https://youtube.com/c/mariyt10" target="_blank" rel="noreferrer"> <SiYoutube></SiYoutube> </a>
           </div>
       </div>
