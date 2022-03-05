ac43c24c1a67175eb18a95ee8088b083b4252940
M2rsh <69349269+M2rsho@users.noreply.github.com>
2022-03-05 18:01:12 +0100

    Update tts.py

--- src/cogs/commands/tts.py
+++ src/cogs/commands/tts.py
@@ -29,7 +29,7 @@ class tts(commands.Cog):
             lang = support.getLanguageFileG(ctx.guild)
             max = 1000
             if len(str(text)) > max:
-                await ctx.send(embed=discord.Embed(description=lang["commands"]["tts"]["maxLenght"].format(max=max), color=support.colours.default))
+                await ctx.reply(embed=discord.Embed(description=lang["commands"]["tts"]["maxLenght"].format(max=max), color=support.colours.default))
                 return
             text = text.split("-l")
             text.append("en")
@@ -42,5 +42,30 @@ class tts(commands.Cog):
             os.remove(file)
 
 
+class tts_slash(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @checks.default()
+    @cooldown(1, support.cooldown, BucketType.user)
+    @commands.slash_command(description=support.getDescription("en.json", "tts"))
+    async def tts(self, ctx, *, text):
+        async with ctx.typing():
+            lang = support.getLanguageFileG(ctx.guild)
+            max = 1000
+            if len(str(text)) > max:
+                await ctx.respond(embed=discord.Embed(description=lang["commands"]["tts"]["maxLenght"].format(max=max), color=support.colours.default))
+                return
+            text = text.split("-l")
+            text.append("en")
+            try:
+                async with timeout(2):
+                    file = await support.processing.tts(f"{text[0]}", f"{text[1]}".replace(" ", ""))
+            except asyncio.TimeoutError:
+                raise TimeoutError("Command Timed out.")
+            await ctx.respond(mention_author=False, file=discord.File(file), content=lang["commands"]["tts"]["returnSuccess"])
+            os.remove(file)
+            
 def setup(bot):
     bot.add_cog(tts(bot))
+    bot.add_cog(tts_slash(bot))
